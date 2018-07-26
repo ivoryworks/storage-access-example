b732a75bd286b59ab555499f30d58ecdbf823465
ivoryworks <ivoryworkz@gmail.com>
2018-07-26 20:04:07 +0900

    内部ストレージにファイルを書き込む

--- app/src/main/java/com/ivoryworks/storageaccesstest/MainActivity.java
+++ app/src/main/java/com/ivoryworks/storageaccesstest/MainActivity.java
@@ -1,15 +1,24 @@
 package com.ivoryworks.storageaccesstest;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.storage.StorageManager;
 import android.os.storage.StorageVolume;
+import android.support.v4.provider.DocumentFile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQ_CODE_PRIMARY_WRITE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +51,37 @@ public class MainActivity extends AppCompatActivity {
             strBuilder.append("\n");
         }
         svText.setText(strBuilder.toString());
+    }
+
+    /**
+     * 内部ストレージに書き込む
+     */
+    public void onPrimaryWrite(View view) {
+        StorageManager storageManager = (StorageManager) getSystemService(this.STORAGE_SERVICE);
+        StorageVolume volume = storageManager.getPrimaryStorageVolume();
+        Intent intent = volume.createAccessIntent(Environment.DIRECTORY_MUSIC);
+        startActivityForResult(intent, REQ_CODE_PRIMARY_WRITE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Uri uri = data.getData();
+        getContentResolver().takePersistableUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        DocumentFile pickedDir = DocumentFile.fromTreeUri(this, uri);
+
+        switch (requestCode) {
+            case REQ_CODE_PRIMARY_WRITE:
+                DocumentFile newFile = pickedDir.createFile("text/plain", "test");
+                try {
+                    OutputStream out = getContentResolver().openOutputStream(newFile.getUri());
+                    out.write("This file is not audio.".getBytes());
+                    out.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
     }
 }
